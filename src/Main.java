2fc359e3d3e23f9820a3c87a4831f61fde64fcb0
Anukool Tiwari <anukool.tiwari@philips.com>
2017-01-13 10:28:55 +0530

    Main class

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,11 @@ public class Main {
 
     public static void main (String [] args)
     {
-       // MySingleton.mySingleton
+        //MySingleton.mySingleton;
 
-       // MySingleton secondInstance = new MySingleton();
+        //MySingleton secondInstance = new MySingleton();
 
-       // MySingleton.getInstance();
+        MySingleton.getInstance();
+
     }
 }
